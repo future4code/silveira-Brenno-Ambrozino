8c9d5e06393468ee01cd6206c4dcf134f9deb1eb
brennoambrozino <brennoambrozino@gmail.com>
2022-02-14 20:56:24 -0300

    aula-de-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,49 @@
+/* 
+Questão 1:
+10
+10, 5 
+
+Questão 2:
+20, 10, 10
+
+Questão 3:
+p = horaDiariaTrabalhada
+t = rendaDiaria
+*/
+
+const nome = prompt("Qual é o seu nome?");
+const idade = Number(prompt("Quantos anos você tem?")); 
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+/* a variavel "nome" é script e a variavel "idade" é number*/
+
+console.log("Olá Brenno, você tem 20 anos");
+
+const pergunta1 = prompt("Você gosta de café?");
+const pergunta2 = prompt("Você acha o céu bonito?");
+const pergunta3 = prompt("Você gosta de frio?");
+
+console.log("Você gosta de café? -", pergunta1);
+console.log("Você acha o céu bonito -?", pergunta2);
+console.log("Você gosta de frio? -", pergunta3);
+
+let a=25;
+let b=10;
+let c;
+
+c=a;
+a=b;
+b=c;
+
+console.log("O novo valor de 'a' é", a);
+console.log("O novo valor de 'b' é", b);
+
+
+
+
+
+
+
+

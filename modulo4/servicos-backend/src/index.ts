9e9e7a88dd994af3c4803606591e945490ff4667
brennoambrozino <brennoambrozino@gmail.com>
2022-07-02 15:53:26 -0300

    servicos-backend

---
+++ modulo4/servicos-backend/src/index.ts
@@ -0,0 +1,16 @@
+import app from "./app"
+import { AddressInfo } from 'net'
+import {getAddress} from "./endpoints/getAddress"
+import { insertAddress } from "./endpoints/insertAddress"
+
+app.get("/endereco/:cep", getAddress)
+app.post("/user/:cep", insertAddress)
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if(server) {
+        const address = server.address() as AddressInfo
+        console.log(`Server is running in http://localhost:${address.port}`)
+    } else {
+        console.error('Failure upon starting server')
+    }
+})
